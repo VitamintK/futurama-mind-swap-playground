3790180d7ec84d280cd9cf91d5a8d1f5d80f90e0
KEvin Wang <kevinwang@kevinwang.us>
2016-01-24 07:43:51 -0800

    added a PURE CSS SINGLE-DIV fullscreen button
    for laughs and giggles I guess.  fullscreen button is a SINGLE DIV who
    is defined by dark box-shadows of 2 shapes (horizontal rectangle, and
    vertical rectangle) duplicated and transposed in 4 ways each to make up
    each corner of the button.

--- futurama-theorem/aesthetics.js
+++ futurama-theorem/aesthetics.js
@@ -1,5 +1,25 @@
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
 function validate(){
-	//if 2 checkboxes are already selected, reset this to unchecked.
+	//if 2 checkboxes are already selected, reset this checkbox to unchecked.
 	checked = 0;
 	for(i = 0; i < bodies.length; i++){
 		if(bodies[i].cb.checked){
@@ -9,7 +29,7 @@ function validate(){
 	if(checked >= 3){
 		this.checked = false;
 	}
-	//just in case more than 2 are selected somehow
+	//just in case more than 2 are selected somehow, just uncheck them all.
 	if(checked > 3){
 		for(i = 0; i<bodies.length; i++){
 			bodies[i].cb.checked = false;
@@ -17,6 +37,7 @@ function validate(){
 	}
 }
 function checkDone(){
+	//if each brain is in the rightful body, return true.
 	for(i = 0; i<bodies.length; i++){
 		if(bodies[i].body_name != bodies[i].brain.name){
 			return false;
@@ -30,6 +51,7 @@ function checkDoneAndFin(){
 	}
 }
 function swap(){
+	//swap the two brains that are in the two checked bodies.
 	checked = [];
 	for(i = 0; i<bodies.length; i++){
 		if(bodies[i].cb.checked){
@@ -61,28 +83,24 @@ var Brain = function(name, element, bg){
 	this.element = element;
 	this.element.style.background = bg;
 }
-//Brain.prototype.moveBody(new_body){
-//
-//}
 var Flesh = function(body_name, brain, element, bg){
 	this.body_name = body_name;
 	this.brain = brain;
 	this.element = element;
 	this.element.style.background = bg;
-	//this.selected = false;
-	//element.addEventListener('click', function(event){this;})
 		cb = document.createElement("input");
 		cb.type = "checkbox";
 		cb.id = 'cb' + body_name;
 		cb.addEventListener('click', validate);
 	this.cb = cb;
 		element.htmlFor = 'cb' + body_name;
-		//console.log(brain);
 		element.appendChild(brain.element);
 	fc.appendChild(cb);
 	fc.appendChild(flesh);
 }
 Flesh.prototype.moveBrain = function(new_flesh){
+	//This should be called swapBrain, actually.
+	//It swaps the brains of two bodies - both DOM elements, and object attributes.
 	this_b = this.brain;
 	that_b = new_flesh.brain;
 	new_flesh.element.appendChild(this.brain.element);
@@ -93,6 +111,7 @@ Flesh.prototype.moveBrain = function(new_flesh){
 setColorButton = function(){
 	c = document.getElementById('colors')
 	if(colorful=="true"){
+		//the colorful class makes the button become colorful.
 		c.className = "colorful";
 		c.title = "click to go back to red and blue when you refresh";
 	} else {
@@ -115,6 +134,7 @@ oneTimeSetup = function(){
 	document.getElementById('restart').addEventListener('click', function(){
 		doEverything();
 	});
+	document.getElementById('fullscreen').addEventListener('click', toggleFullScreen);
 	setColorButton();
 }
 doEverything = function(){
@@ -125,13 +145,16 @@ doEverything = function(){
 	green = Math.floor(Math.random()*100);
 	blue = Math.floor(Math.random()*100);
 	num = 6;
+	//
+	//remove the "disappear" class from the swap button, to make it visible again.
 	document.getElementById("swap").className = "";
+	//remove all the old bodies from the DOM.
 	fc = document.getElementById("flesh-container");
 	while (fc.firstChild) {
     	fc.removeChild(fc.firstChild);
 	}
-
-	
+	//
+	//fill the bgs array with strings representing rgb() color values for css.
 	for(k = num/-2; k < 1 + (num/2); k++){
 		//bgs.push("rgb("+Math.floor(Math.min(Math.random()*100)+average,255)+","+50+","+50+")");
 		if(colorful=="true"){
@@ -142,27 +165,23 @@ doEverything = function(){
 		}
 	}
 	shuffle(bgs);
+	//fill the brains array with brain objects
 	for(j = 0; j < 6; j++){
 		br = document.createElement("x-brain");
 		brains.push(new Brain(j, br, bgs[j]));
 	}
 	shuffle(brains);
-	
+	//fill the bodies array with flesh objects.
 	for(i = 0; i < 6; i++){
-		
 		flesh = document.createElement("label");
-		//flesh.htmlFor = 'cb' + i;
-		//flesh.appendChild(document.createTextNode("yolo"));
-		//flesh.style.background = bgs[i];
-
 		f = new Flesh(i,brains[i],flesh, bgs[i]);
 		bodies.push(f);
 	}
 	//one-time under here
 	sb = document.getElementById("swap");
 	sb.addEventListener('click', swap);
-	
 }
+//main
 var colorful = localStorage.getItem("colorful");
 if(colorful == null){colorful = "false";}
 	var brains = [];
